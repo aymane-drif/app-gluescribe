b90b7e62ec1c338ec6b275752e4d16243c2e0d4b
Aymane <aymane.drif.22@gmail.com>
2025-02-19 21:08:51 +0100

    first commit

---
+++ pages/auth/callback.vue
@@ -0,0 +1,17 @@
+<!-- File: pages/auth/callback.vue -->
+<template>
+    <div>
+        <h1>Authentication Callback</h1>
+        <p>Processing authentication response...</p>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        const query = this.$route.query;
+        // Process the Supabase auth response here (e.g., token handling)
+        console.log('Auth query params:', query);
+    }
+}
+</script>
